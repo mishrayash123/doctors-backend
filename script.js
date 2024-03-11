02e2bc487c6f0d8f605322e21c3f17655ecaf2f4
Prameela17 <prameela.10t@gmail.com>
2024-03-11 22:48:54 +0530

    Project Added

---
+++ script.js
@@ -0,0 +1,38 @@
+const buttons = document.querySelectorAll(".all");
+
+// function openNavbar() {
+// 	document.getElementById("sideNavigationBar")
+// 		.style.width = "50%";
+// }
+// function closeNavbar() {
+// 	document.getElementById("sideNavigationBar")
+// 		.style.width = "0%";
+// }
+
+buttons.forEach(button=>{
+    button.addEventListener("click",()=>{
+        buttons.forEach(btn=>{
+            btn.classList.remove("selected");
+        });
+        button.classList.add("selected");
+    });
+});
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  var MenuItems = document.getElementById('MenuItems');
+  MenuItems.style.maxHeight = '0px';
+
+  function menutoggle() {
+    if (MenuItems.style.maxHeight == '0px') {
+      MenuItems.style.maxHeight = '200px';
+    } else {
+      MenuItems.style.maxHeight = '0px';
+    }
+  }
