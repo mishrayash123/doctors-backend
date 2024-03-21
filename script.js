16c9e6965f9393208010e1da7d9f95da89c11a7f
Prameela17 <prameela.10t@gmail.com>
2024-03-21 20:54:10 +0530

    made changes

--- script.js
+++ script.js
@@ -17,9 +17,6 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
-var MenuItems = document.getElementById('MenuItems');
-MenuItems.style.maxHeight = '0px';
-
 function menutoggle() {
     if (MenuItems.style.maxHeight == '0px') {
         MenuItems.style.maxHeight = '200px';
@@ -27,3 +24,31 @@ function menutoggle() {
         MenuItems.style.maxHeight = '0px';
     }
 }
+
+document.getElementById("ent_specialist").addEventListener("click", function(){
+    window.location.href = "doctors.html"
+})
+
+document.getElementById("endocrinologis").addEventListener("click", function(){
+    window.location.href = "doctors.html"
+})
+
+document.getElementById("oncologists").addEventListener("click", function(){
+    window.location.href = "doctors.html"
+})
+
+document.getElementById("pulmonologist").addEventListener("click", function(){
+    window.location.href = "doctors.html"
+})
+
+document.getElementById("endocrinologis").addEventListener("click", function(){
+    window.location.href = "doctors.html"
+})
+
+document.getElementById("veterinarian").addEventListener("click", function(){
+    window.location.href = "doctors.html"
+})
+
+document.getElementById("radiologist").addEventListener("click", function(){
+    window.location.href = "doctors.html"
+})
