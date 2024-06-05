0e71d672e783ab4b5f403099519796342ff5de63
yash kumar mishra <mishrayash3778@gmail.com>
2024-06-05 10:06:55 +0530

    kdk

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -71,16 +71,3 @@ export const register = async (req, res) => {
     return res.sendStatus(400);
   }
 }
-
-
-export const deleteUser = async (req, res) => {
-  try {
-    const { id } = (req.params);
-    const deletedUser = await deleteUserById(id);
-
-    return res.json(deletedUser);
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
-  }
-}
